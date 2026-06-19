b8b0f011d21d498da0e87889d58ec8553401df0c
adarsh <adarshdeshmukh00000@gmail.com>
2026-06-19 10:37:14 +0530

    all sidebar elements positioned and added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import SideBar from './Components/sidebar'
 
 function App() {
   return (
     <>
-    
+        <SideBar></SideBar>
     </>
   )
 }
---
+++ frontend/src/Components/sidebar.jsx
@@ -0,0 +1,52 @@
+import Logo from "../assets/Site Assets/Logo.png"
+import DefaultPFP from "../assets/Site Assets/DefaultUserPhoto.png"
+
+function SideBar()
+{
+  return (
+    <>
+      <div className="h-dvh bg-[#0A0A0F] w-64 flex flex-col">
+
+        <div  className="h-25 pl-4 pt-7.5">
+          <div className="flex">
+            <div className="h-10 w-10"><img src={Logo} alt="Logo" /></div>
+          </div>
+        </div>
+
+        <div className="h-18 pl-4 pt-4 border-t border-gray-700">
+          <div className="flex">
+            <div className="h-10 w-10"><img src={DefaultPFP} alt="pfp" /></div>
+          </div>
+        </div>
+        <hr></hr>
+
+        <div className="text-[#FFFFFF] text-[16px] pl-4 border-t border-gray-700">
+          <a href="#Dashboard" className="flex h-10 pt-4">
+            <div>Dashboard</div>
+          </a>
+          <a href="#components" className="flex h-10 pt-4">
+            <div>Components</div>
+          </a>
+          <a href="#tools" className="flex h-10 pt-4">
+            <div>Tools</div>
+          </a>
+          <a href="#History" className="flex h-10 pt-4">
+            <div>History</div>
+          </a>
+          <a href="#reports" className="flex h-10 pt-4">
+            <div>Reports</div>
+          </a>
+          <a href="#notifications" className="flex h-10 pt-4">
+            <div>Notifications</div>
+          </a>
+        </div>
+
+        <div className="flex pl-4 text-red-600 border-t border-gray-700 mt-auto">
+          <div>Log Out</div>
+        </div>
+
+      </div>
+    </>
+  )
+}
+export default SideBar
