a56de721ed4281e1c31182afb23d07bb881d1a45
adarsh <adarshdeshmukh00000@gmail.com>
2026-06-27 00:43:46 +0530

    initialized Tools page

---
+++ frontend/src/Components/ToolsPage.jsx
@@ -0,0 +1,17 @@
+import {useState} from 'react';
+import {Search ,Menu ,X} from 'lucide-react';
+import SideBar from '/sidebar.jsx';
+import SearchBar from '/searchBar.jsx';
+
+
+
+function ToolsPage() {
+  return (
+    <>
+
+
+
+    </>
+  )
+}
+export default ToolsPage
